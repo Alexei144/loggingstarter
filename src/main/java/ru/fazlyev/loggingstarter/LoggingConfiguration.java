c4a243501f6bd818de8e3eacc0c90764aa6fa0aa
Alexei Fazlyev <alexeifer@yandex.ru>
2024-07-01 09:38:48 +0300

    initial commit

---
+++ src/main/java/ru/fazlyev/loggingstarter/LoggingConfiguration.java
@@ -0,0 +1,10 @@
+package ru.fazlyev.loggingstarter;
+
+public class LoggingConfiguration {
+
+    public static void testLog(String message) {
+        System.out.println("Вызвано из logging starter'a: " + message);
+    }
+
+
+}
